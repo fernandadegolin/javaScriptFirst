c49f990abe77228a3658f1eec6c81e3e1423b930
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-07-20 19:10:28 -0300

    Add files via upload

--- #06 - Controle de Gastos/src/script.js
+++ #06 - Controle de Gastos/src/script.js
@@ -1,14 +1,54 @@
-let valorSaldo = 20;
+//let valor = localStorage.getItem('saldo');
+//localStorage.setItem('saldo', 2);
 
-function pegarSaldo(){
+let valorSaldo = 0;
+
+function pegarSaldo() {
     let texto = document.querySelector('.valor');
-    texto.innerHTML =`R$${valorSaldo}`;
+    texto.innerHTML = `R$${valorSaldo}`;
 
-    if(valorSaldo.indexOf('-')!== -1){
-
-    } else{
-        
-    }
+    validaCor(texto);
 }
 
 pegarSaldo();
+
+function validaCor(texto){
+    if (String(valorSaldo).indexOf('-') !== -1) {
+        texto.style.color = 'red';
+
+    } else {
+        texto.style.color = 'green';
+    }
+}
+
+function atualizarSaldo(type){
+
+    if(type === 'diminuir'){
+
+        let valor = document.querySelector('#despesa').value;
+        valor === '' || valor === null ? valor = 0 : true; //Validação do Campo Númerico (Operaçao Ternaria)
+
+        valorSaldo = parseInt(valorSaldo) - parseInt(valor);
+
+        pegarSaldo();
+
+        document.querySelector('#despesa').value = '';
+
+    } else if (type === 'adicionar'){
+        let valor = document.querySelector('#receita').value;
+        valor === '' || valor === null ? valor = 0 : true; //Validação do Campo Númerico (Operaçao Ternaria)
+
+        valorSaldo = parseInt(valorSaldo) + parseInt(valor);
+
+        pegarSaldo();
+        document.querySelector('#receita').value = '';
+
+
+    } else{
+
+
+    }
+
+}
+
+
