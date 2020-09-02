3c23d59f669d221b339778a5876a44cefb0f3b45
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-09-02 14:28:20 -0300

    Update script.js

--- #07 - Fica! Vai ter bolo/src/script.js
+++ #07 - Fica! Vai ter bolo/src/script.js
@@ -1,23 +1,4 @@
-// let nome = Number(prompt("Digite um numero?"));
-// let n1 = 2
-// let soma = nome + n1
-
-// alert(`É um grande prazer ter você ${soma}`)
-
-
-/* Testando o OPERADOR TERNARIO */
-// let n1 = 1;
-// let n2 = '1';
-// let resultado = n1 === n2 ? 'LOGICO QUE NAO' : 'OPA é SIM';
-
-
-/* Testanto os SELETORES */
-// let p1 = document.getElementsByTagName('h1');
-// document.write(`${p1.innerHTML} <=`)
-
-
 /* Testando EVENTOS */
-
 
 let a = document.getElementById('area');
 
