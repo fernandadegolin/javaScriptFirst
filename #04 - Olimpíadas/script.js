b0d2ec70848fab8034d744e5934553132b1cc5b5
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-11-12 12:11:34 -0300

    Update script.js

--- #04 - Olimpíadas/script.js
+++ #04 - Olimpíadas/script.js
@@ -2,7 +2,6 @@ var primeiraOlimpiada = 1896;
 
 var anoAtual= parseInt(prompt("Em que ano estamos?"));
 
-
 // Condicao
 while(primeiraOlimpiada <= anoAtual) {
   document.write('Em ' + primeiraOlimpiada + " tivemos Olimpíadas!" + '<br>');
